deb71f6326ba0b7799db8bd167427bb6efab2d72
Jonathan Raiman <jonathanraiman@gmail.com>
2012-02-29 01:52:18 -0800

    fixed some date and time issues

--- app.rb
+++ app.rb
@@ -4,9 +4,8 @@ require 'net/http/persistent'
 require 'rexml/document'
 include REXML
 
-@@data = []
+@@data = [{"latitude":48.858391,"longitude":2.341461, "content":"At 8PM : How's it going!","cityname":"Paris"},{"latitude":45.757942,"longitude":4.822998, "content":"At 9PM : Yo!","cityname":"Lyon"}]
 @@count = 0
-@locationIP = "chupacabra"
 
 get '/' do
   File.read(File.join('public', 'index.html'))
@@ -19,10 +18,11 @@ end
 
 post '/messages' do
   content_type :json
-  apple = "hey"
+  time = Time.new
+  
   @locationIP = request.ip
   jsonresponse = JSON.parse(Net::HTTP.get_response(URI.parse("http://api.ipinfodb.com/v3/ip-city/?key=fcf5c6d5f16bd594c40883ac6161f788ac985f3d674ffee79667dbeac5a087c8&ip="+@locationIP+"&format=json")).body)
-  message = JSON.parse(request.body.read.to_s).merge(:id => @@count +=1, :user_ip => request.ip, :cityname => jsonresponse['cityName'], :latitude => jsonresponse['latitude'], :longitude => jsonresponse['longitude'])
+  message = JSON.parse(request.body.read.to_s).merge(:id => @@count +=1, :receptiontime => time.strftime("%H:%M:%S"), :user_ip => request.ip, :cityname => jsonresponse['cityName'], :latitude => jsonresponse['latitude'], :longitude => jsonresponse['longitude'])
   @@data << message
   message.to_json
 end
--- public/application.js
+++ public/application.js
@@ -8,19 +8,25 @@ var messages = new MessageStore;
 
 var userlocations = [];
 
+function pad2(number) {
+   
+     return (number < 10 ? '0' : '') + number
+   
+}
+
 var MessageView = Backbone.View.extend({
 
    events: { "submit #chatForm" : "handleNewMessage" }
 
   , handleNewMessage: function(data) {
   	var currentTime = new Date();
-  	var seconds = currentTime.getSeconds();
-  	var minutes = currentTime.getMinutes();
-  	var hours = currentTime.getHours();
-  	var month = currentTime.getMonth() + 1;
-	var day = currentTime.getDate();
+  	var seconds = pad2(currentTime.getSeconds());
+  	var minutes = pad2(currentTime.getMinutes());
+  	var hours = pad2(currentTime.getHours());
+  	var month = pad2(currentTime.getMonth() + 1);
+	var day = pad2(currentTime.getDate());
 	var year = currentTime.getFullYear();
-    var inputField ="At "+hours+":"+minutes+":"+seconds+" : "+$('input[name=newMessageString]').val();
+    var inputField =$('input[name=newMessageString]').val();
     $('input[name=newMessageString]').val('');
     $("#chatHistory").animate({ scrollTop: $("#chatHistory").attr("scrollHeight") }, 250);
     messages.create({content: inputField});
@@ -31,9 +37,9 @@ var MessageView = Backbone.View.extend({
   	var i=0;
     var data = messages.map(function(message) {
     	//$("#chatterbox").attr('src','http://maps.googleapis.com/maps/api/staticmap?sensor=false&size=200x200&center='+message.get('latitude')+','+message.get('longitude')+'&zoom=12&style=feature:road.local%7Celement:geometry%7Chue:0x00ff00%7Csaturation:100&style=feature:landscape%7Celement:geometry%7Clightness:-100');
+    	var uniquelocation = true;
     	for (var l=0; l< userlocations.length; l++)
     		{
-    		var uniquelocation = true;
     		if (userlocations[l] == (message.get('latitude')+","+message.get('longitude')))
     			{
     			uniquelocation = false;
@@ -45,17 +51,17 @@ var MessageView = Backbone.View.extend({
     		userlocations[i] = message.get('latitude')+","+message.get('longitude');
     		i++;
     		}
-    	return message.get('content') + '\n(' + message.get('cityname')+ ')\n' 
+    	return message.get('receptiontime') + message.get('content') + '</br><a>' + message.get('cityname')+ '</a></br>' 
     	});
     var result = data.reduce(function(memo,str) { return memo + str }, '');
     if (rerenderusers == true)
     	{
     	for (var h=0; h<userlocations.length; h++)
     		{
-    		$("#usersbox").append("<img src='http://maps.googleapis.com/maps/api/staticmap?sensor=false&size=200x50&center="+userlocations[h]+"&zoom=12&style=feature:road.local%7Celement:geometry%7Chue:0x00ff00%7Csaturation:100&style=feature:landscape%7Celement:geometry%7Clightness:-100' />");
+    		$("#usersbox").append("<img src='http://maps.googleapis.com/maps/api/staticmap?center="+userlocations[h]+"&zoom=2&format=png&sensor=false&size=300x100&maptype=roadmap&style=feature:road%7Csaturation:-99%7Clightness:-49&style=feature:water%7Csaturation:-27%7Clightness:-56&style=feature:transit.station%7Cvisibility:off&style=element:labels%7Cvisibility:off%7Cinvert_lightness:true%7Cgamma=0.32&style=element:feature:landscape.man_made%7Csaturation:-85%7Chue:0x00ffa2%7Clightness:56%7Cvisibility:off&style=feature:landscape%7Chue:0xf6ff00%7Csaturation:-99%7Cgamma:9.99&style=feature:poi.park%7Csaturation:45%7Clightness:-19%7Cgamma:0.99%7Chue:0x88ff00&style=feature:road.local%7Cgamma:0.99%7Chue:0x3bff00%7Csaturation:-55%7Clightness:-23' />");
     		}
     	}
-    $("#chatHistory").text(result);
+    $("#chatHistory").html(result);
     return this;
   }
 
