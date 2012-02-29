01682918ed997cbfbe4ea04843a2157c2ecd7c15
Jonathan Raiman <jonathanraiman@gmail.com>
2012-02-29 02:58:33 -0800

    finishing touches to ui

--- public/application.js
+++ public/application.js
@@ -19,13 +19,6 @@ var MessageView = Backbone.View.extend({
    events: { "submit #chatForm" : "handleNewMessage" }
 
   , handleNewMessage: function(data) {
-  	var currentTime = new Date();
-  	var seconds = pad2(currentTime.getSeconds());
-  	var minutes = pad2(currentTime.getMinutes());
-  	var hours = pad2(currentTime.getHours());
-  	var month = pad2(currentTime.getMonth() + 1);
-	var day = pad2(currentTime.getDate());
-	var year = currentTime.getFullYear();
     var inputField =$('input[name=newMessageString]').val();
     $('input[name=newMessageString]').val('');
     $("#chatHistory").animate({ scrollTop: $("#chatHistory").attr("scrollHeight") }, 250);
@@ -50,7 +43,7 @@ var MessageView = Backbone.View.extend({
     		userlocations[i] = message.get('latitude')+","+message.get('longitude');
     		i++;
     		}
-    	return message.get('receptiontime') + message.get('content') + '</br><a>' + message.get('cityname')+ '</a></br>' 
+    	return '<span class="timeicon">'+message.get('receptiontime')+'</span>' + message.get('content') + '</br><img class="pin" src="location_pin.png"/><a>' + message.get('cityname')+ '</a></br>' 
     	});
     var result = data.reduce(function(memo,str) { return memo + str }, '');
     var usersbox = "";
@@ -58,8 +51,14 @@ var MessageView = Backbone.View.extend({
     		{
     		usersbox= usersbox+"<img src='http://maps.googleapis.com/maps/api/staticmap?center="+userlocations[h]+"&zoom=2&format=png&sensor=false&size=300x100&maptype=roadmap&style=feature:road%7Csaturation:-99%7Clightness:-49&style=feature:water%7Csaturation:-27%7Clightness:-56&style=feature:transit.station%7Cvisibility:off&style=element:labels%7Cvisibility:off%7Cinvert_lightness:true%7Cgamma=0.32&style=element:feature:landscape.man_made%7Csaturation:-85%7Chue:0x00ffa2%7Clightness:56%7Cvisibility:off&style=feature:landscape%7Chue:0xf6ff00%7Csaturation:-99%7Cgamma:9.99&style=feature:poi.park%7Csaturation:45%7Clightness:-19%7Cgamma:0.99%7Chue:0x88ff00&style=feature:road.local%7Cgamma:0.99%7Chue:0x3bff00%7Csaturation:-55%7Clightness:-23' />";
     		}
-    $("#usersbox").html(usersbox);
-    $("#chatHistory").html(result);
+    if ($("#usersbox").html() != usersbox)
+    	{
+    	$("#usersbox").html(usersbox);
+    	}
+    if ($("#usersbox").html() != result)
+    	{
+        $("#chatHistory").html(result);
+        }
     return this;
   }
 
