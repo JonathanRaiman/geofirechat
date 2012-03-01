34abad65a543549921e92933b5ae2f971aebe060
Jonathan Raiman <jonathanraiman@gmail.com>
2012-02-29 18:20:52 -0800

    fixed some loops

--- app.rb
+++ app.rb
@@ -4,7 +4,7 @@ require 'net/http/persistent'
 require 'rexml/document'
 include REXML
 
-@@data = [{"latitude" => 48.858391,"longitude" => 2.341461, "receptiontime" => "21:00:18", "content" => "How's it going!","cityname" => "Paris", "countryname" => "France"},{"latitude" => 45.757942,"longitude" => 4.822998, "content" => "Yo!","cityname" => "Lyon", "countryname" => "France", "receptiontime" => "23:42:00"}]
+chunks = [{"latitude" => 48.858391,"longitude" => 2.341461, "receptiontime" => "21:00:18", "content" => "How's it going!","cityname" => "Paris", "countryname" => "France"},{"latitude" => 45.757942,"longitude" => 4.822998, "content" => "Yo!","cityname" => "Lyon", "countryname" => "France", "receptiontime" => "23:42:00"}]
 @@count = 0
 
 get '/' do
@@ -12,10 +12,11 @@ get '/' do
 end
 
 get '/messages' do 
-	@@data.each do
-	message["receptiontime"] = "5"
+  chunks.each do
+  	chunk["receptiontime"]="5"
+  end
   content_type :json
-  @@data.to_json
+  chunks.to_json
 end
 
 post '/messages' do
@@ -25,7 +26,7 @@ post '/messages' do
   @locationIP = request.ip
   jsonresponse = JSON.parse(Net::HTTP.get_response(URI.parse("http://api.ipinfodb.com/v3/ip-city/?key=fcf5c6d5f16bd594c40883ac6161f788ac985f3d674ffee79667dbeac5a087c8&ip="+@locationIP+"&format=json")).body)
   message = JSON.parse(request.body.read.to_s).merge(:id => @@count +=1, :receptiontime => Time.now, :displayed_time => relative_time(Time.now),:user_ip => request.ip, :cityname => (jsonresponse['cityName']).split(" ").each{|word| word.capitalize!}.join(" "), :countryname => (jsonresponse['countryName']).split(" ").each{|word| word.capitalize!}.join(" "), :latitude => jsonresponse['latitude'], :longitude => jsonresponse['longitude'])
-  @@data << message
+  chunks << message
   message.to_json
 end
 
